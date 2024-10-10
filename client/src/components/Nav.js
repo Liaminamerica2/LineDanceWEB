af6b2f37bc3c8b9b62183d4821e562b8e5a1a992
teem lim <liamgottesman@gmail.com>
2024-10-10 16:53:38 -0700

    final

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,24 +1,22 @@
-import React from 'react'; // ES6 js
-import {Link} from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Nav() {
-    return(
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark top">
-            <img src="..\images\boots-ezgif.com-webp-to-jpg-converter.jpg" />
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark top">
+            <img src="../images/boots-ezgif.com-webp-to-jpg-converter.jpg" alt="Logo" />
 
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navMainMenu" aria-controls="navMainMenu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                <p>Hello World!</p>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navMainMenu" aria-controls="navMainMenu" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
 
-            
-            <div class="navbarbuttons">
-                {/* <Link to=''></> */}
+            <div className="navbarbuttons collapse navbar-collapse" id="navMainMenu">
                 <Link to='/' className="nav-item nav-link active">Home</Link>
-                <Link to='/tweets' className="nav-item nav-link">Tweets</Link>
+                <Link to='/campaign' className="nav-item nav-link">Campaign</Link>
+                <Link to='/dancesearch' className="nav-item nav-link">DanceSearch</Link>
             </div>
-            
+
+            <p>Hello World!</p>
         </nav>
     );
 }
